4cd3f220478b97845eb552fb5e6e1c4d097c1a0a
Rosemary Tuttle <rosemary.brinton@gmail.com>
2016-09-15 20:41:01 -0700

    modified java file, added css file

--- app.js
+++ app.js
@@ -1,44 +1,62 @@
 var userName = prompt('What\'s your name?');
 alert ('Welcome to my site ' + userName + '!');
 
-var questionOne = prompt('Was I born in WA?');
-questionOne = questionOne.toLowerCase();
-if(questionOne === 'yes' || questionOne === 'y') {
+var answerOne = prompt('Was I born in WA?');
+answerOne = answerOne.toLowerCase();
+if(answerOne === 'yes' || answerOne === 'y') {
   alert ('Nope! Guess again!');
-} else if(questionOne === 'no' || questionOne === 'n') {
+} else if(answerOne === 'no' || answerOne === 'n') {
   alert ('That\'s right, I was born in CA, and moved to Northern Idaho when I was 2!');
 } else {
   alert ('Try typing no or n.');
 }
 
-var questionTwo = prompt('Do I like to snowboard?');
-questionTwo = questionTwo.toLowerCase()
-if(questionTwo === 'yes' || questionTwo === 'y') {
+var answerTwo = prompt('Do I like to snowboard?');
+answerTwo = answerTwo.toLowerCase();
+if(answerTwo === 'yes' || answerTwo === 'y') {
   alert ('Yes! It\'s my favorite thing to do!.');
-} else if(questionTwo === 'no' || questionTwo === 'n') {
+} else if(answerTwo === 'no' || answerTwo === 'n') {
   alert ('Skiing is fun too... I do both!');
 } else {
   alert ('Maybe is wrong answer. I LOVE winter sports!');
 }
 
-var questionThree = prompt('Do I like movies?');
-questionThree = questionThree.toLowerCase()
-if(questionThree === 'yes' || questionThree === 'y') {
+var answerThree = prompt('Do I like movies?');
+answerThree = answerThree.toLowerCase();
+if(answerThree === 'yes' || answerThree === 'y') {
   alert ('Yes, actually, The Departed is my favorite movie.');
-} else if(questionThree === 'no' || questionThree === 'n') {
+} else if(answerThree === 'no' || answerThree === 'n') {
   alert ('Reading is cool too.');
 } else {
   alert ('Welcome to boredom.');
 }
 
-var questionFour = prompt('Did I live in Colorado for 4 years?');
-questionFour= questionFour.toLowerCase()
-if(questionFour === 'yes' || questionFour === 'y') {
+var answerFour = prompt('Did I live in Colorado for 4 years?');
+answerFour = answerFour.toLowerCase();
+if(answerFour === 'yes' || answerFour === 'y') {
   alert ('Yep! You are correct.');
-} else if(questionFour === 'no' || questionFour === 'n') {
+} else if(answerFour === 'no' || answerFour === 'n') {
   alert ('I wish I could clone myself and live in two places at the same time!');
 } else {
   alert ('If you aren\'t sure, check out my LinkedIn page!');
+}
 
+var answerFive = prompt('Did I go to school in Montana?');
+answerFive = answerFive.toLowerCase();
+if(answerFive === 'yes' || answerFive === 'y') {
+  alert ('Yes, MT is down to earth.');
+} else if(answerFour === 'no' || answerFive === 'n') {
+  alert ('There is no other place I would rather be.');
+} else {
+  alert ('MT is the only answer.');
+}
 
+var answerSix = prompt('Did I go to school in Montana?');
+answerSix = answerFive.toLowerCase();
+if(answerSix === 'yes' || answerSix === 'y') {
+  alert ('Yes, MT is down to earth.');
+} else if(answerSix !== 'yes' && answerFive !== 'y') {
+  alert ('There is no other place I would rather be.');
+} else {
+  alert ('MT is the only answer.');
 }
